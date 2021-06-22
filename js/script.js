ce5f29a2e1eeec29998b3930500e6481c3a1e483
sara_h <narwhals925@gmail.com>
2021-06-21 22:55:30 -0400

    Modified some variable names and commented out the logged out responses

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ const repoList = document.querySelector(".repo-list");
 const gitUserInfo = async function () {
     const userInfo = await fetch(`https://api.github.com/users/${username}`);
     const data = await userInfo.json();
-    console.log(data);
+    //console.log(data);
     displayUserInfo(data);
 };
 
@@ -29,23 +29,21 @@ const displayUserInfo = function (data) {
         </div>
     `;
     overview.append(div);
+    gitRepos();
 };
 
 const gitRepos = async function () {
     const fetchRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per-page=100`);
     const repoData = await fetchRepos.json();
-    console.log(repoData);
-    displayRepos(repos);
+    //console.log(repoData);
+    displayRepos(repoData);
 };
-//gitRepos ();
 
 const displayRepos = function (repos) {
     for (const repo of repos) {
-        let reposItem = document.createElement("li");
-        reposItem.classList.add(".repo");
-        reposItem.innerHTML = `
-        <h3>${repoData.name}</h3>
-        `;
-    repoList.append(reposItem);
+        let repoItem = document.createElement("li");
+        repoItem.classList.add(".repo");
+        repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+        repoList.append(reposItem);
     }
 };
